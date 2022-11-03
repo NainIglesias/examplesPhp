29856f85affebda96f107f997866b36a900fd7a5
kant003 <gonzalezm.angel@gmail.com>
2022-11-03 11:47:09 +0100

    ejemplo12

--- ejem06.php
+++ ejem06.php
@@ -20,19 +20,17 @@ echo "La división es ".$division;
 echo "<br/>";
 
 if($num1 > $num2){
-    echo "El numero1 es mayor que el num2";
+    echo "El numero1 es mayor que el numero2";
 }else{
     echo "El numero1 es menor que el numero2";
 }
 echo "<br/>";
 //$num1 = 5
 //$num2 = 6
-if($num1 > $num2  &&  $num1 != 13){
-    echo "El numero1 es mayor que el num2";
+if($num1 > $num2  ||  $num1 != 13){
+    echo "El numero1 es mayor que el numero2";
 }else{
     echo "El numero1 es menor que el numero2";
 }
-
-
 
 ?>
